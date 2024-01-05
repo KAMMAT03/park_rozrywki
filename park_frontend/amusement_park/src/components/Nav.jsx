e2aa14daa002f0b39431cab8ac7d3d106ce7505e
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2024-01-05 02:05:10 +0100

    nav adjustments

--- park_frontend/amusement_park/src/components/Nav.jsx
+++ park_frontend/amusement_park/src/components/Nav.jsx
@@ -1,27 +1,33 @@
 import React from "react";
+import "../styles/style.css";
 
 export default function Nav() {
-    return (
-        <nav className=" fixed top-0 left-0 right-0 flex gap-4 bg-emerald-700 h-24 items-center p-8 shadow-md">
-            <p className=" mr-auto text-slate-200 font-bold text-xl">Adrenaline City</p>
-            <button
-                className=" bg-slate-200 text-emerald-700 px-2 py-1 font-bold rounded-lg hover:opacity-85
-                transition duration-200 linear active:opacity-75"
-            >
-                Bilety
-            </button>
-            <button
-                className=" bg-slate-200 text-emerald-700 px-2 py-1 font-bold rounded-lg hover:opacity-85
-                transition duration-200 linear active:opacity-75"
-            >
-                Parking
-            </button>
-            <button
-                className=" bg-slate-200 text-emerald-700 px-2 py-1 font-bold rounded-lg hover:opacity-85
-                transition duration-200 linear active:opacity-75"
-            >
-                Zaloguj
-            </button>
-        </nav>
-    )
+  return (
+    <nav
+      className="fixed left-0 right-0 top-0 z-10 flex h-24 items-center gap-4 bg-[#725AC1] p-8 shadow-md"
+      id="nav"
+    >
+      <p className=" mr-auto text-2xl font-bold text-slate-200">
+        Adrenaline City
+      </p>
+      <button
+        className=" linear rounded-lg bg-slate-200 px-2 py-1 font-bold text-slate-800
+                shadow-xl transition duration-200 hover:opacity-85 active:opacity-75"
+      >
+        Bilety
+      </button>
+      <button
+        className=" linear rounded-lg bg-slate-200 px-2 py-1 font-bold text-slate-800
+                shadow-xl transition duration-200 hover:opacity-85 active:opacity-75"
+      >
+        Parking
+      </button>
+      <button
+        className=" linear rounded-lg bg-slate-200 px-2 py-1 font-bold text-slate-800
+                shadow-xl transition duration-200 hover:opacity-85 active:opacity-75"
+      >
+        Zaloguj
+      </button>
+    </nav>
+  );
 }
