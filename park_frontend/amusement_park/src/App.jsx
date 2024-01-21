83d66d233ac3160dc51c3282decca3743cdabdfe
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2024-01-21 14:39:47 +0100

    implemented all page routes

--- park_frontend/amusement_park/src/App.jsx
+++ park_frontend/amusement_park/src/App.jsx
@@ -4,16 +4,20 @@ import Main from "./pages/Main";
 import Bilety from "./pages/Bilety";
 import Auth from "./pages/Auth";
 import Tables from "./pages/Tables";
+import NotFound from "./pages/Redirect";
+import Account from "./pages/Account";
 import "./index.css";
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="*" element={<Main />} />
-        {/* <Route path="*" element={<Bilety />} /> */}
-        {/* <Route path="*" element={<Auth />} /> */}
-        {/* <Route path="*" element={<Tables />} /> */}
+        <Route path="/main" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/tickets" element={<Bilety />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/adminview" element={<Tables />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </div>
   );
